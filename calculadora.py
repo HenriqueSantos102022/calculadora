02780560100612cad5fae79b10e5e52d68d48495
Henrique <henriques.santod@gmail.com>
2025-05-24 16:33:50 -0300

    feat: adicionar função de soma

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+def soma(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    x = int(input("Digite o primeiro número: "))
+    y = int(input("Digite o segundo número: "))
+    print("Resultado da soma:", soma(x, y))
