9252bdc21a4ed65c2e8c4e7656754963f8677b89
Henrique <henriques.santod@gmail.com>
2025-05-24 16:41:37 -0300

    feat: adicionar função de subtração

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,12 @@
 def soma(a, b):
     return a + b
 
+def subtrai(a, b):
+    return a - b
+
 if __name__ == "__main__":
     x = int(input("Digite o primeiro número: "))
     y = int(input("Digite o segundo número: "))
+    
     print("Resultado da soma:", soma(x, y))
+    print("Resultado da subtração:", subtrai(x, y))
