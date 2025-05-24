4844def0919c36987f1172105f9eaa91636dadcc
Henrique <henriques.santod@gmail.com>
2025-05-24 16:48:58 -0300

    feat: adicionar função de multiplicação

--- calculadora.py
+++ calculadora.py
@@ -4,9 +4,13 @@ def soma(a, b):
 def subtrai(a, b):
     return a - b
 
+def multiplica(a, b):
+    return a * b
+
 if __name__ == "__main__":
     x = int(input("Digite o primeiro número: "))
     y = int(input("Digite o segundo número: "))
     
     print("Resultado da soma:", soma(x, y))
     print("Resultado da subtração:", subtrai(x, y))
+    print("Resultado da multiplicação:", multiplica(x, y))
